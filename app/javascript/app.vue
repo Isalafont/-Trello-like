f72718eabf80d8edb210d071282d289d8deea385
Isabelle Lafont <isalafont@gmail.com>
2020-12-22 12:08:03 +0100

    Add Card to lists / Persist cards into db

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,25 +1,57 @@
 <template>
   <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
+    <div v-for="(list, index) in original_lists" class="col-3">
       <h6>{{ list.name }}</h6>
       <hr />
 
       <div v-for="(card, index) in list.cards" class="card card-body">
         {{ card.name }}
       </div>
+      <div class="card card-body">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button v-on:click="submitMessages(list.id)" class="btn btn-primary"">Add</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+  import Rails from '@rails/ujs';
+  export default {
     props: ["original_lists"],
-}
+    data: function() {
+      return {
+        messages: {},
+        lists: this.original_lists,
+      }
+    },
+
+    methods: {
+      submitMessages: function(list_id) {
+        var data = new FormData
+        data.append("card[list_id]", list_id)
+        data.append("card[name]", this.messages[list_id])
+
+        Rails.ajax({
+          url: "/cards",
+          type: "POST",
+          data: data,
+          dataType: "json",
+          success: (data) => {
+            const index = this.lists.findIndex(item => item.id == list_id)
+            this.lists[index].push(data)
+            this.messages[list_id] = undefined
+          }
+        })
+      }
+    }
+  }
+
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
 </style>
