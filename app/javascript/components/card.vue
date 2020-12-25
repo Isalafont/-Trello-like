eec6eaff85038b8f80a01b1d1fb80bf8da1bd54d
Isabelle Lafont <isalafont@gmail.com>
2020-12-25 17:25:25 +0100

    Add card component

---
+++ app/javascript/components/card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card card-body mb-3">
+    {{ card.name }}
+  </div>
+</template>
+
+<script>
+  // import Rails from '@rails/ujs';
+
+  export default {
+    props: ['card'],
+    data: function() {
+      return {
+        editing: false,
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+</style>
