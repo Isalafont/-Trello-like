ce441c70054bd18056bed649debf18ee08864a6e
Isabelle Lafont <isalafont@gmail.com>
2020-12-25 18:00:36 +0100

    fix error

--- app/javascript/components/card.vue
+++ app/javascript/components/card.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="card card-body mb-3">
+    <div @click="editing=true" class="card card-body mb-3">
       {{ card.name }}
     </div>
 
     <div v-if="editing" class="modal-backdrop show"></div>
-    <div v-if="editing" @click="closeModal" class="modal show" style="display: block;">
+    <div v-if="editing" @click="closeModal" class="modal show" style="display: block">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -32,7 +32,13 @@
     data: function() {
       return {
         editing: false,
+        name: this.card.name,
       }
+    },
+
+    methods: {
+      closeModal: function() {},
+      save: function() {},
     }
   }
 
