43029d7f925e657e6e00dea08d9e47794c82a6ee
Isabelle Lafont <isalafont@gmail.com>
2021-01-11 20:00:38 +0100

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-listSeed = List.create({ id: 1, name: 'This is a list', position: 1 })
-cardSeed = Card.create({ id: 1, name: 'This is a card', position: 1 })
+listSeed = List.create({ name: 'This is a list' })
+cardSeed = Card.create({ name: 'This is a card' })
