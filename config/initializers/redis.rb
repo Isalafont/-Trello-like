4da5b85f7f485df1f6223951b12dd2ea414c313b
Isabelle Lafont <isalafont@gmail.com>
2022-01-11 12:15:34 +0100

    Change Redis variable

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-Redis.current = Redis.new(url:  ENV['REDIS_URL'],
+redis_url = ENV['REDIS_URL'] || 'redis://redis:6379'
+
+Redis.current = Redis.new(url:  ENV['redis_url'],
                           port: ENV['REDIS_PORT'],
                           db:   ENV['REDIS_DB'])
