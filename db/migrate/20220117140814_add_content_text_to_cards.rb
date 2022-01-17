af6a683a7206b17e5c1ccd7de8b285a178ed0aef
Isabelle Lafont <isalafont@gmail.com>
2022-01-17 15:16:48 +0100

    Install JS librairies + Cards migration
    Install simplemde to install Markdown
    Install inline-attachment to attach File and picture to content text
    
    Add a migration to Cards => Content:text

---
+++ db/migrate/20220117140814_add_content_text_to_cards.rb
@@ -0,0 +1,5 @@
+class AddContentTextToCards < ActiveRecord::Migration[6.1]
+  def change
+    add_column :cards, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_125301) do
+ActiveRecord::Schema.define(version: 2022_01_17_140814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_125301) do
     t.integer "position"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "content"
     t.index ["list_id"], name: "index_cards_on_list_id"
   end
 
