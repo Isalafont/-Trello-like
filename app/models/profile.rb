921f01895acf79e2566b1d64d11e264fd9e0be0a
Isabelle Lafont <isalafont@gmail.com>
2022-01-25 11:58:47 +0100

    Add case sensitive spec

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :first_name, :last_name, :username, presence: true, length: { minimum: 3 }
+  validates :first_name, :last_name, :username, presence: true
+  validates :username, length: { minimum: 3 }
   validates :username, uniqueness: { case_sensitive: false }
   validates :bio, length: { maximum: 500,
                             too_long: "%{count} characters is the maximum allowed" }
